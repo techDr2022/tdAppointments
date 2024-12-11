e847704ca69a27ba4aadbe3d39bd9316cc18c5da
techDr2022 <info@techdr.in>
2024-12-11 19:39:24 +0530

    BullMQ  update

--- components/Hematologybmt.tsx
+++ components/Hematologybmt.tsx
@@ -332,7 +332,7 @@ const Hematologybmt = () => {
       }
     } else {
       toast.error("An unexpected error occurred");
-      console.error("Service or Doctor ID  missing.");
+      console.error("Service or Doctor ID missing.");
     }
   };
 
